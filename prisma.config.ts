3a49f1ecc699f9a860d3cbfba6b87c4907d78f90
okolijerry <142677067+okolijerry@users.noreply.github.com>
2025-10-27 05:18:31 +0100

    seed

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,10 +1,3 @@
 import { defineConfig } from "@prisma/config";
 
-export default defineConfig({
-  seed: {
-    run: async () => {
-      const { main } = await import("./prisma/seed");
-      await main();
-    },
-  },
-});
+export default defineConfig({});
